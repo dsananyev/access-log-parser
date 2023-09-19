2151ff6b5f65d8f612f21fce53bcc96aa89da93b
d.ananev <ds.ananyev@gmail.com>
2023-09-20 00:01:07 +0300

    added two numbers calculation

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+
+        System.out.println("Введите первое число: ");
+      int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+      int secondNumber = new Scanner(System.in).nextInt();
+
+      int sum = firstNumber + secondNumber;
+        System.out.println("Сумма:" + sum);
+      int difference = firstNumber - secondNumber;
+        System.out.println("Разность: " + difference);
+      int product = firstNumber * secondNumber;
+        System.out.println("Произведение: " + product);
+      double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Частное: " + quotient);
+
     }
 }
