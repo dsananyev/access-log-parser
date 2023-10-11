b57818023ad50ac8384d2e8df6333f90ed825205
d.ananev <ds.ananyev@gmail.com>
2023-10-12 00:22:04 +0300

    -added infinite cycle to search for a file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        Scanner sc = new Scanner(System.in);
+        int pathCount = 0;
+
+        while (true) {
+            System.out.println("Укажите путь к файлу");
+            String path = sc.nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (isDirectory) {
+                System.out.println("Указан путь к папке, а не к файлу\n");
+                continue;
+            } else if (!fileExists) {
+                System.out.println("Указан путь до несуществующего файла\n");
+                continue;
+            } else if (fileExists) {
+                pathCount += 1;
+                System.out.println("Путь указан верно. Это файл N:" + pathCount + "\n");
+                continue;
+            }
+        }
     }
 }
